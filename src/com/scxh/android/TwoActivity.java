2093f3060f367dce6e89425677330c3397545e40
viktor.zhou <2915858910@qq.com>
2015-06-10 11:26:02 +0800

    first commit layout

---
+++ src/com/scxh/android/TwoActivity.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.scxh.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author Administrator
+ *
+ */
+public class TwoActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.two_activity);
+		
+	}
+}
---
+++ src/com/scxh/android/layout/LinerLayoutAcitiy.java
@@ -0,0 +1,16 @@
+package com.scxh.android.layout;
+
+import com.scxh.android.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LinerLayoutAcitiy extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_liner_layout);
+		
+	}
+}
