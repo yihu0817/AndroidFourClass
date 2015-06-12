b1d99d62e321797c144f9c34a73bae52b4f3c293
viktor.zhou <2915858910@qq.com>
2015-06-12 11:42:50 +0800

    textview

--- src/com/scxh/android/layout/InfalterAcitivty.java
+++ src/com/scxh/android/layout/InfalterAcitivty.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 
 import com.scxh.android.R;
 
@@ -11,7 +12,7 @@ public class InfalterAcitivty extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+		TextView tx = new TextView(this);
 //		FrameLayout layout = (FrameLayout)findViewById(android.R.id.content);
 //		layout.setBackgroundColor(getResources().getColor(android.R.color.holo_blue_bright));
 		
